6037e59157a7c4552fd7dce2eb69c1e3cfcfde24
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-02 11:02:10 +0530

    News api configuration added.

--- src/main/java/com/newsAPI/config/NewsApiConfig.java
+++ src/main/java/com/newsAPI/config/NewsApiConfig.java
@@ -1,4 +1,17 @@
 package com.newsAPI.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
 public class NewsApiConfig {
+
+    @Value("${news.api.url}")
+    private String apiUrl;
+
+    @Value("${news.api.key}")
+    private String apiKey;
+
 }
