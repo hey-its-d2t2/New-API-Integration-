60a469aaf00d23840af1b58ac43b4c8b8de60024
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-02 11:13:42 +0530

    DTO defined for news and source

---
+++ src/main/java/com/newsAPI/dto/NewsDto.java
@@ -0,0 +1,15 @@
+package com.newsAPI.dto;
+
+import lombok.Data;
+
+@Data
+public class NewsDto {
+    private Source source;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+}
---
+++ src/main/java/com/newsAPI/dto/Source.java
@@ -0,0 +1,9 @@
+package com.newsAPI.dto;
+
+import lombok.Data;
+
+@Data
+public class Source {
+    private String id;
+    private String name;
+}
