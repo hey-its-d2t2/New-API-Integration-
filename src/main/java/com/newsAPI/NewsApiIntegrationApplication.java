ec3639bf7d82f1a39f9c8f48ea6091a4d7cb26ef
Deepak Singh <deepsinghkumar01@gmail.com>
2024-10-02 10:54:23 +0530

    Initial commit: project created.

---
+++ src/main/java/com/newsAPI/NewsApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.newsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsAPI/NewsApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
